d14ffd03cfe5597839fb1077ff10401b2eea3236
Átila Camurça <camurca.home@gmail.com>
2025-01-09 10:41:07 -0300

    Background: Curral à esquerda Casa do Papai Noel
    Close #16

---
+++ tiled/Gameboy-Forest-Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Gameboy-Forest-Tileset" tilewidth="8" tileheight="8" tilecount="480" columns="24">
+ <image source="Gameboy-Forest-Tileset.png" width="192" height="160"/>
+</tileset>
