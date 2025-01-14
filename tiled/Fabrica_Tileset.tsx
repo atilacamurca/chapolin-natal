0306cbd262a01bf5be8095148c905cf05faf0aa2
Átila Camurça <camurca.home@gmail.com>
2025-01-14 17:30:48 -0300

    Background: Lado de dentro da Fábrica do Papai Noel
    Closes #18

---
+++ tiled/Fabrica_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Fabrica_Tileset" tilewidth="8" tileheight="8" tilecount="320" columns="16">
+ <image source="Fabrica_Tileset.png" width="128" height="160"/>
+</tileset>
--- tiled/RPG_interior.tsx
+++ tiled/RPG_interior.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="RPG_interior" tilewidth="8" tileheight="8" tilecount="286" columns="22">
  <image source="RPG_interior.png" width="176" height="104"/>
+ <tile id="265" probability="10"/>
+ <tile id="266" probability="10"/>
+ <tile id="267" probability="10"/>
+ <tile id="269" probability="10"/>
+ <tile id="272" probability="10"/>
 </tileset>
