d674999e3d75381a95b4957b287d738ed177134f
Átila Camurça <camurca.home@gmail.com>
2025-01-23 11:18:38 -0300

    Background: Lado esquerdo da Casa da Bruxa Baratuxa
    close #29

--- tiled/gameboy_outdoor_assets_v1.2.tsx
+++ tiled/gameboy_outdoor_assets_v1.2.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="gameboy_outdoor_assets_v1.2" tilewidth="8" tileheight="8" tilecount="840" columns="30">
  <image source="gameboy_outdoor_assets_v1.2.png" width="240" height="224"/>
+ <tile id="724" probability="50"/>
 </tileset>
---
+++ tiled/gameboy_rpg_v09.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="gameboy_rpg_v09" tilewidth="8" tileheight="8" tilecount="3040" columns="95">
+ <image source="gameboy_rpg_v09.png" width="763" height="256"/>
+</tileset>
