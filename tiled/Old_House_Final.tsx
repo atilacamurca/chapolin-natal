8a0d78df7fa431cf6cad0510a196451f5a44b5cf
Átila Camurça <camurca.home@gmail.com>
2025-05-06 11:55:32 -0300

    Esconderijo do Tripa Seca

---
+++ tiled/Old_House_Final.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Old_House_Final" tilewidth="8" tileheight="8" tilecount="126" columns="14">
+ <image source="Old_House_Final.png" width="112" height="72"/>
+</tileset>
