6ddfa2a29875c066a328c7dad2274ddbafce49e3
Átila Camurça <camurca.home@gmail.com>
2025-06-27 12:07:46 -0300

    Tileset Estrela Cadente, para ser usada nos créditos
    Para dar ideia de que há movimentação continua da Cena

---
+++ tiled/Estrela_1_T.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Estrela_1_T" tilewidth="8" tileheight="8" tilecount="16" columns="8">
+ <image source="Estrela_1_T.png" width="64" height="16"/>
+</tileset>
